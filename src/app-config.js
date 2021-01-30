0c94666910d1a7561085b747122eadfd479d402b
Momchil Ignatov <momchil.ignatov@sap.com>
2021-01-30 17:16:51 +0200

    Testing

--- src/app-config.js
+++ src/app-config.js
@@ -1,8 +1,10 @@
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
-export const REMOTE_BANK_API = CORS_PROXY + "https://serene-escarpment-42577.herokuapp.com/api"
+export const REMOTE_BANK_API = CORS_PROXY + "https://lifeblood-bloodbank.herokuapp.com/api"
+export const REMOTE_ACCOUNTS_API = CORS_PROXY + "https://lifeblood-accounts.herokuapp.com/api"
+
 export const LOCAL_BLOOD_BANK_API = "http://localhost:5001/api"
 export const LOCAL_ACCOUNT_SERVICE_API = "http://localhost:4200/accounts"
 
-export const BLOOD_BANK_API = LOCAL_BLOOD_BANK_API
-export const ACCOUNT_SERVICE_API = LOCAL_ACCOUNT_SERVICE_API
+export const BLOOD_BANK_API = REMOTE_BANK_API
+export const ACCOUNT_SERVICE_API = REMOTE_ACCOUNTS_API
